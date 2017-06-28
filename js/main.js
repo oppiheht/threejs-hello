2df63bf9bd2d0e9a09b458e7e9ae0d29c81b35e5
Brandon Bussjaeger <oppiheht@gmail.com>
2017-06-28 15:18:45 -0700

    added robots, moved floor

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ gui.add(camera.position, 'x').listen();
 gui.add(camera.position, 'y').listen();
 gui.add(camera.position, 'z').listen();
 
-camera.position.set(27000, 35000, -14407);
-camera.lookAt(new THREE.Vector3(30000, 0, 13000));
+camera.position.set(-57000, 37000, -27000);
+camera.lookAt(new THREE.Vector3(-37000, 0, -27000));
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth - 3, window.innerHeight - 3);
@@ -26,20 +26,20 @@ function randomColor() {
   return '#'+r+g+b;
 }
   
-let RACK_HEIGHT = 2000;
+let RACK_HEIGHT = 1828.8;
 racks.forEach(function(rack, index) {
-  let rackGeom = new THREE.BoxBufferGeometry(rack.length, RACK_HEIGHT, rack.depth);
-  rackGeom.applyMatrix( new THREE.Matrix4().makeTranslation( rack.length / 2, RACK_HEIGHT / 2, rack.depth / 2));
+  let rackGeom = new THREE.BoxBufferGeometry(-rack.length, RACK_HEIGHT, rack.depth);
+  rackGeom.applyMatrix( new THREE.Matrix4().makeTranslation( -rack.length / 2, RACK_HEIGHT / 2, -rack.depth / 2));
   let rackMaterial = new THREE.MeshBasicMaterial({ color: randomColor() });
   let rackMesh = new THREE.Mesh(rackGeom, rackMaterial);
-  rackMesh.position.x = rack.origin[0];
+  rackMesh.position.x = -rack.origin[0];
   rackMesh.position.z = rack.origin[1];
   rackMesh.rotation.y = (rack.angle * Math.PI / 180);
   scene.add(rackMesh);
 });
 
-let WAREHOUSE_SIZE = 100000;
-let FLOOR_CHECKER_SIZE = 100000;
+let WAREHOUSE_SIZE = 200000;
+let FLOOR_CHECKER_SIZE = 200000;
 let segments = WAREHOUSE_SIZE / FLOOR_CHECKER_SIZE;
 let materials = [new THREE.MeshBasicMaterial({ color: 0x666666}), new THREE.MeshBasicMaterial({ color: 0x777777})];
 let floorGeom = new THREE.PlaneGeometry(WAREHOUSE_SIZE, WAREHOUSE_SIZE, segments, segments);
@@ -51,10 +51,21 @@ for (let x = 0; x < segments; x++) {
   }
 }
 let floor = new THREE.Mesh(floorGeom, materials);
-floor.position.x = 25000;
+floor.position.x = -30000;
+floor.position.y = -5;
 floor.position.z = 20000;
 floor.rotation.x = (-Math.PI / 2);
 scene.add(floor);
+
+
+let robotGeom = new THREE.BoxBufferGeometry(640, 500, 620);
+let robotMaterial = new THREE.MeshBasicMaterial({color: 0x0000ee});
+for (let i = 0; i < 30; i++) {
+    let robot = new THREE.Mesh(robotGeom, robotMaterial);
+    scene.add(robot);
+    robot.position.x = 1000;
+    robot.position.z = 1000 * i;
+}
 
 let render = function () {
   requestAnimationFrame(render);
