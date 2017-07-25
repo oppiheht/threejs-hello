bad74afb8618434bc757c6324f7362c9c0b9ec9b
Brandon Bussjaeger <oppiheht@gmail.com>
2017-07-25 11:41:32 -0700

    Cleaned up search module and added public search functions

--- js/ui/tools/searchModule.js
+++ js/ui/tools/searchModule.js
@@ -1,6 +1,5 @@
 (function(wm3d) {
   
-
   let _input = null;
   document.addEventListener('keyup', _onKeyUp);
 
@@ -12,62 +11,71 @@
 
   function _addSearchBar() {
     _input = document.createElement('input');
-
     _input.style.position = 'absolute';
-
     document.body.appendChild(_input);
-
     _input.style.top = window.innerHeight / 10 + 'px';
     _input.style.left = (window.innerWidth - _input.scrollWidth) / 2 + 'px';
-
     _input.focus();
-
-    _input.addEventListener('keydown', _search);
+    _input.addEventListener('keydown', _keyDown);
   }
 
-  function _search(event) {
+  function _keyDown(event) {
     event.stopPropagation();
     if (event.code == 'Escape' || event.code == 'Enter') {
       if (event.target.value.length == 0) {
-        wm3d.scene.children.forEach((child) => {
-          if (child.outline) {
-            wm3d.scene.remove(child.outline);
-            child.outline = null;
-          }
-        })
+        clearSearchedItems();
       }
-      _input.removeEventListener('keydown', _search);
+      _input.removeEventListener('keydown', _keyDown);
       _input.remove();
       _input = null;
-      return;
     }
+    else {
+      search(event.target.value + event.key);
+    }
+  }
 
+  function clearSearchedItems() {
+    wm3d.scene.children.forEach((child) => _removeOutline(child));
+  }
+
+  function search(text) {
     let count = 0;
     wm3d.scene.children.forEach((child) => {
-      if (child.searchable != false && child.name.toUpperCase().includes((event.target.value + event.key).toUpperCase())) {
+      if (child.searchable != false && child.name.toUpperCase().includes(text.toUpperCase())) {
         count++;
-
         if (!child.outline) {
-          let outlineMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
-          let outline = new THREE.Mesh(child.geometry, outlineMaterial);
-          outline.position.copy(child.position);
-          outline.rotation.copy(child.rotation);
-          outline.scale.multiplyScalar(1.07);
-          child.outline = outline;
-          outline.name = child.name;
-          outline.searchable = false;
-          wm3d.scene.add(outline);
+          _outlineMesh(child);
         }
       }
-      else if (child.outline) {
-        wm3d.scene.remove(child.outline);
-        child.outline = null;
+      else {
+        _removeOutline(child);
       }
     });
     console.log('Found '+count+' matching search items');
   }
 
+  function _removeOutline(parentMesh) {
+    if (parentMesh.outline) {
+      wm3d.scene.remove(parentMesh.outline);
+      parentMesh.outline = null;
+    }
+  }
+
+  function _outlineMesh(mesh) {
+    let outlineMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    let outline = new THREE.Mesh(mesh.geometry, outlineMaterial);
+    outline.position.copy(mesh.position);
+    outline.rotation.copy(mesh.rotation);
+    outline.scale.multiplyScalar(1.07);
+    mesh.outline = outline;
+    outline.name = mesh.name;
+    outline.searchable = false;
+    wm3d.scene.add(outline);
+  }
+
   wm3d.searchModule = {
+    search: search,
+    clearSearchedItems: clearSearchedItems,
   };
 
 })(wm3d);
