e684bfd3000905c48e71498746d025a90b570391
Brandon Bussjaeger <oppiheht@gmail.com>
2017-09-28 07:56:34 -0700

    Locks change color to red when they have an owner set

--- js/objects/lock.js
+++ js/objects/lock.js
@@ -2,6 +2,9 @@
     
     // lockName -> Mesh
     let _locks = {};
+    
+    let _ownedColor = 0x993333;
+    let _unownedColor = 0x339933;
 
     function addLock(name, points) {
         if (!points || points.length < 3) {
@@ -17,7 +20,7 @@
 
         let extrudeSettings = {steps: 1, amount: -200};
         let lockGeometry = new THREE.ExtrudeGeometry(lockShape, extrudeSettings);
-        let lockMaterial = new THREE.MeshBasicMaterial({color: 0x339933});
+        let lockMaterial = new THREE.MeshBasicMaterial({color: _unownedColor});
         let lockMesh = new THREE.Mesh(lockGeometry, lockMaterial);
         lockMesh.setRotationFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI/2);
         lockMesh.baseName = name;
@@ -34,8 +37,10 @@
         }
         if (owner) {
             lock.name = lock.baseName + ": " + owner;
+            lock.material.setValues({color: _ownedColor});
         } else {
             lock.name = lock.baseName;
+            lock.material.setValues({color: _unownedColor});
         }
         return lock.name;
     }
